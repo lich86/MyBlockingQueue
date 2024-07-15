ad549961f119f293d84f58323f07c2d210ce4835
lich <elizaveta.chervonnaya@gmail.com>
2024-07-15 17:01:31 +0300

    add essential logic

---
+++ src/main/java/com/chervonnaya/Main.java
@@ -0,0 +1,39 @@
+package com.chervonnaya;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+        MyBlockingQueue<Integer> queue = new MyBlockingQueue<>(5);
+
+        ExecutorService executor = Executors.newFixedThreadPool(10);
+
+        Runnable producer = () -> {
+            for (int i = 0; i < 10; i++) {
+                try {
+                    System.out.println("Produced: " + i);
+                    queue.enqueue(i);
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                }
+            }
+        };
+
+        Runnable consumer = () -> {
+            for (int i = 0; i < 10; i++) {
+                try {
+                    int item = queue.dequeue();
+                    System.out.println("Consumed: " + item);
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                }
+            }
+        };
+
+        executor.execute(producer);
+        executor.execute(consumer);
+
+        executor.shutdown();
+    }
+}
---
+++ src/main/java/com/chervonnaya/MyBlockingQueue.java
@@ -0,0 +1,35 @@
+package com.chervonnaya;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class MyBlockingQueue<E> {
+
+    private Queue<E> queue = new LinkedList<E>();
+    private final int maxCapacity;
+
+    public MyBlockingQueue(int maxCapacity) {
+        this.maxCapacity = maxCapacity;
+    }
+
+    public synchronized void enqueue(E e) throws InterruptedException {
+        while (queue.size() == maxCapacity) {
+            wait();
+        }
+        queue.add(e);
+        notifyAll();
+    }
+
+    public synchronized E dequeue() throws InterruptedException {
+        if(queue.isEmpty()) {
+            wait();
+        }
+        E item = queue.poll();
+        notifyAll();
+        return item;
+    }
+
+    public synchronized int size() {
+        return queue.size();
+    }
+}
